f78cac4bdb795c3e1deb7cff2bf2dda2f96507d4
utso1707097 <utsomollik1707097@gmail.com>
2023-09-20 01:02:37 +0600

    CRUD functionality implemented in project app

---
+++ projects/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import project,projects,createProject,updateProject,deleteProject
+
+urlpatterns = [
+    path('',projects,name="projects"),
+    path('project/<str:pk>/',project,name="project"),
+    path('create-project/',createProject,name="create-project"),
+    path('update-project/<str:pk>/',updateProject,name="update-project"),
+    path('delete-project/<str:pk>/',deleteProject,name="delete-project")
+]
